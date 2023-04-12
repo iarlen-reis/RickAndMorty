233bfd03093b6be1886c804ce6ef777f6c5b5e4d
= <iarlensr@hotmail.com>
2023-04-11 22:21:53 -0300

    feat: creating about page and adding style.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,10 +2,52 @@ import React from "react";
 
 import { ContainerStyled } from "../../styles/Global";
 
+import { AboutStyled, InfoStyled, SectionStyled, ImageStyled } from "./styles";
+
+import ImageAbout from "/about/protal-2.png";
+
 function About() {
   return (
     <ContainerStyled>
-      <h1>Página About</h1>
+      <AboutStyled>
+        <InfoStyled>
+          <SectionStyled>
+            <h1>O que é rick and Morty?</h1>
+            <p>
+              Rick and Morty é uma animação da Cartoon Network, que mostra a
+              história de um avó (Rick) e seu neto (Morty) que fazem diversas
+              viagens tempo-espaço e por universos paralelos.
+            </p>
+          </SectionStyled>
+          <SectionStyled>
+            <h1>Qual o intuito do site?</h1>
+            <p>
+              O intuito da aplicação é exibir informações sobre a animação,
+              como, por exemplo, listar todos os personagens existentes na
+              animação. Na aplicação também é possível ver os detalhes de cada
+              personagem, como, sua espécie, seu gênero e seu status atual.
+            </p>
+          </SectionStyled>
+          <SectionStyled>
+            <h2>Qual a API utilizada?</h2>
+            <p>
+              A aplicação utiliza a API do{" "}
+              <a
+                href="https://rickandmortyapi.com/documentation"
+                target="_blank"
+                rel="noreferrer"
+              >
+                RickAndMortyAPI
+              </a>{" "}
+              para buscar as informações disponibilizadas no site, a API é feita
+              no formato REST e GraphQL.
+            </p>
+          </SectionStyled>
+        </InfoStyled>
+        <ImageStyled>
+          <img src={ImageAbout} alt="" />
+        </ImageStyled>
+      </AboutStyled>
     </ContainerStyled>
   );
 }
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,10 +1,97 @@
 import styled from "styled-components";
 
-export const AboutContainerStyled = styled.div`
+export const AboutStyled = styled.div`
   width: 100%;
-  background: ${({ theme }) => theme.colors.primary};
+  min-height: 100vh;
+  display: flex;
+  flex-direction: row;
+
+  padding-top: 3rem;
+
+  gap: ${({ theme }) => theme.gap.regular};
+
+  @media (max-width: 1070px) {
+    flex-direction: column;
+
+    gap: ${({ theme }) => theme.gap.small};
+  }
+`;
+
+export const InfoStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: ${({ theme }) => theme.gap.big};
+`;
+
+export const SectionStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  gap: ${({ theme }) => theme.gap.regular};
+
+  color: ${({ theme }) => theme.colors.white};
+
+  h1,
+  h2 {
+    font-family: ${({ theme }) => theme.family.itim};
+  }
 
   h1 {
+    font-size: ${({ theme }) => theme.font.superBig};
+  }
+
+  h2 {
+    font-size: ${({ theme }) => theme.font.big};
+  }
+
+  p {
+    max-width: 700px;
+    font-size: ${({ theme }) => theme.font.regular};
+  }
+
+  a {
+    font-weight: 700;
+
+    padding: 0.1rem 0.6rem;
+    border-radius: 4px;
+
+    cursor: pointer;
+
     color: ${({ theme }) => theme.colors.white};
+    background: ${({ theme }) => theme.colors.secundary};
+
+    transition: 0.5s;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.tertiary};
+    }
+  }
+
+  @media (max-width: 520px) {
+    h1 {
+      font-size: ${({ theme }) => theme.font.big};
+    }
+
+    h2 {
+      font-size: ${({ theme }) => theme.font.superRegular};
+    }
+
+    p {
+      font-size: ${({ theme }) => theme.font.small};
+    }
+  }
+`;
+
+export const ImageStyled = styled.div`
+  width: 40%;
+
+  img {
+    max-width: 100%;
+  }
+
+  @media (max-width: 1070px) {
+    width: 80%;
+    margin: 0 auto;
   }
 `;
